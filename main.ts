eb1e3ae86ec5b3f54a8feb6410b33a59c2602093
ZackMC14 <prany97426@gmail.com>
2025-07-15 17:58:47 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@
 namespace sensors {
 
     export enum Direction {
-        //% block="forward"
-        Forward = 1,
-        //% block="reverse"
-        Reverse = 0,
-        //% block="stop"
+        //% block="ทวนเข็มนาฬิกา"
+        CounterClockwise = 0,
+        //% block="ตามเข็มนาฬิกา"
+        Clockwise = 1,
+        //% block="หยุด"
         Stop = 2
     }
 
-    //% blockId=DDMmultiMotor block="ควบคุมมอเตอร์ 4 ตัว | M1 Dir %m1Dir| M2 Dir %m2| M3 Dir %m3| M4 Dir %m4| ความเร็ว M1 %m1Speed| ความเร็ว M2 %m2Speed| ความเร็ว M3 %m3Speed| ความเร็ว M4 %m4Speed"
+    //% blockId=DDMmultiMotor block="ควบคุมมอเตอร์ 4 ตัว | M1 ทิศทาง %m1Dir| M2 ทิศทาง %m2Dir| M3 ทิศทาง %m3Dir| M4 ทิศทาง %m4Dir| ความเร็ว M1 %m1Speed| ความเร็ว M2 %m2Speed| ความเร็ว M3 %m3Speed| ความเร็ว M4 %m4Speed"
     //% m1Speed.min=0 m1Speed.max=255
     //% m2Speed.min=0 m2Speed.max=255
     //% m3Speed.min=0 m3Speed.max=255
@@ -19,13 +19,11 @@ namespace sensors {
         m1Dir: Direction, m2Dir: Direction, m3Dir: Direction, m4Dir: Direction,
         m1Speed: number, m2Speed: number, m3Speed: number, m4Speed: number
     ): void {
-        // กำหนดพินทิศทางตามคำขอ
-        pins.digitalWritePin(DigitalPin.P1, m1Dir == Direction.Forward ? 1 : 0)
-        pins.digitalWritePin(DigitalPin.P2, m2Dir == Direction.Forward ? 1 : 0)
-        pins.digitalWritePin(DigitalPin.P13, m3Dir == Direction.Forward ? 1 : 0)
-        pins.digitalWritePin(DigitalPin.P15, m4Dir == Direction.Forward ? 1 : 0)
+        pins.digitalWritePin(DigitalPin.P1, m1Dir == Direction.Clockwise ? 1 : 0)
+        pins.digitalWritePin(DigitalPin.P2, m2Dir == Direction.Clockwise ? 1 : 0)
+        pins.digitalWritePin(DigitalPin.P13, m3Dir == Direction.Clockwise ? 1 : 0)
+        pins.digitalWritePin(DigitalPin.P15, m4Dir == Direction.Clockwise ? 1 : 0)
 
-        // กำหนดพินความเร็วโดย map ค่า 0-255 เป็น PWM 0-1023
         pins.analogWritePin(AnalogPin.P8, m1Dir == Direction.Stop ? 0 : pins.map(m1Speed, 0, 255, 0, 1023))
         pins.analogWritePin(AnalogPin.P12, m2Dir == Direction.Stop ? 0 : pins.map(m2Speed, 0, 255, 0, 1023))
         pins.analogWritePin(AnalogPin.P14, m3Dir == Direction.Stop ? 0 : pins.map(m3Speed, 0, 255, 0, 1023))
